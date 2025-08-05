68be8e7f759f8116e9f2d8799a03c1cd4b337008
zainsubhani <96117969+zainsubhani@users.noreply.github.com>
2025-08-05 13:58:52 +0200

    added model

--- ML/MLConcept.py
+++ ML/MLConcept.py
@@ -1,13 +1,20 @@
 from sklearn.datasets import load_iris;
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
 iris = load_iris()
 X=iris.data
 Y=iris.target
 X, y = load_iris(return_X_y=True)
+knn=KNeighborsClassifier(n_neighbors=3) #because 3 number of flowers in dataset
 
 feature_names = iris.feature_names
 target_names = iris.target_names
 X_train, X_test, y_train, y_test = train_test_split(X, y , test_size=0.5)
 print(X_train.shape)
 print(X_test.shape)
+knn.fit(X_train,y_train)
+y_pred=knn.predict(X_test)
+print(metrics.accuracy_score(y_test,y_pred),"checking accurency")
+
 
