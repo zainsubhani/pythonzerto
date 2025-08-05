a9cd6be8db85ba403fefe3303d0224c869280209
zainsubhani <96117969+zainsubhani@users.noreply.github.com>
2025-08-05 14:28:21 +0200

    adding sample

--- ML/MLConcept.py
+++ ML/MLConcept.py
@@ -16,7 +16,7 @@ knn=KNeighborsClassifier(n_neighbors=3) #because 3 number of flowers in dataset
 
 feature_names = iris.feature_names
 target_names = iris.target_names
-X_train, X_test, y_train, y_test = train_test_split(X, y , test_size=0.5)
+X_train, X_test, y_train, y_test = train_test_split(X, y , test_size=0.1)
 print(X_train.shape)
 print(X_test.shape)
 knn.fit(X_train,y_train)
@@ -24,3 +24,11 @@ y_pred=knn.predict(X_test)
 print(metrics.accuracy_score(y_test,y_pred),"checking accurency")
 
 
+# test size is small trade off how much data is for training and testing
+# to improve add more columns
+# we can use different classifier like decision tree classifier 
+
+sample = [[4,4,5,6],[4,4,5,6]]
+prediction = knn.predict(sample)
+pred_speics= [iris.target_names[p] for p in prediction]
+print(pred_speics)
