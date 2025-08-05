33d638e4d7ace8fddbe45a0404b69bdb939f0a70
zainsubhani <96117969+zainsubhani@users.noreply.github.com>
2025-08-05 13:59:35 +0200

    comment

--- ML/MLConcept.py
+++ ML/MLConcept.py
@@ -1,3 +1,9 @@
+#1 import data
+#2 clean the data
+#3 split the data trainingset/test set
+#4 create a model import algo like use library
+#5 check the output
+
 from sklearn.datasets import load_iris;
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
